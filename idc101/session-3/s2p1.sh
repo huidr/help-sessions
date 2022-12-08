f361fddcf3259de203c670fcd1651218ec62cacf
Ronald Huidrom <ronhuidrom@gmail.com>
2022-12-09 05:01:55 +0530

    Add files via upload

---
+++ idc101/session-3/s2p1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Enter a natural number"
+read a
+
+b=0
+
+while [ $a != 0 ];
+do
+    y=$((a%10))
+    b=$((b*10+y))
+    a=$((a/10))
+done
+
+echo "The reversed number is: $b"
---
+++ idc101/session-3/s2p2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Enter a string"
+read string
+
+len=${#string}
+max=$((len-1))
+
+for (( v=$max; v>= 0; v-- )) # note the C++ style syntax
+do
+    s=${string:v:1}
+    echo -n "$s"
+done
+
+echo
+exit 0
+
+# for v in {0..$max} wont work in the above case
